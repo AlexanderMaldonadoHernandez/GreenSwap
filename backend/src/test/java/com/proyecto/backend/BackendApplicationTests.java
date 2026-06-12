f1bc306ddb158297b8e1a1823a4eb891fd685bed
ericbarrjal <ericbarrerav1@gmail.com>
2026-06-12 16:14:18 -0600

    Update BackendApplicationTests.java

--- backend/src/test/java/com/proyecto/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/proyecto/backend/BackendApplicationTests.java
@@ -1,11 +1,13 @@
-package com.greenswap;
+package com.proyecto.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-public class GreenSwapApplication {
+@EntityScan(basePackages = "com.greenswap.model") // <- Esto le dice a Spring dónde buscar a "Articulo"
+public class BackendApplication {
     public static void main(String[] args) {
-        SpringApplication.run(GreenSwapApplication.class, args);
+        SpringApplication.run(BackendApplication.class, args);
     }
 }

43eb5d1127b8d1f9609232f7c457eab0d1aee41a
ericbarrjal <ericbarrerav1@gmail.com>
2026-06-12 17:10:23 -0600

    Create UsuarioRepository.java

---
+++ backend/src/main/java/com/proyecto/backend/repository/UsuarioRepository.java
@@ -0,0 +1,9 @@
+package com.proyecto.backend.repository;
+
+import com.proyecto.backend.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    Optional<Usuario> findByCorreoElectronico(String correoElectronico);
+}

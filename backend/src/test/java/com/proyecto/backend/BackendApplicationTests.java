d36ee5bfcdaa4d7b893db325a9c734685f258cfc
ericbarrjal <ericbarrerav1@gmail.com>
2026-06-12 14:46:07 -0600

    Update BackendApplicationTests.java

--- backend/src/test/java/com/proyecto/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/proyecto/backend/BackendApplicationTests.java
@@ -1,13 +1,11 @@
-package com.proyecto.backend;
+package com.greenswap;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootTest
-class BackendApplicationTests {
-
-    @Test
-    void contextLoads() {
+@SpringBootApplication
+public class GreenSwapApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GreenSwapApplication.class, args);
     }
-
 }
